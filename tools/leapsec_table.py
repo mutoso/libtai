b9fcb0aedb9bb6a7635f626047d2ff886b41d521
Alastair Feille <alastair@muto.so>
2015-05-31 03:29:22 -0500

    Update leapsec_table.py

--- tools/leapsec_table.py
+++ tools/leapsec_table.py
@@ -18,6 +18,8 @@ def convert(time):
         t += 65536 * 65536
     return t
 
+# ============= leapsecsat_unix =============
+
 print """unsigned int leapsecsat_unix(time_t date)
 {
     #define TABLE_LEN """ + str(len(times)) + """
@@ -43,6 +45,7 @@ print """    #undef new_leapsec
     return leapsec_diff[0];
 }"""
 
+# ============= leapsecsat_tai =============
 print
 
 print """unsigned int leapsecsat_tai(tai_t date)
@@ -68,4 +71,32 @@ print """    #undef new_leapsec
     #undef TABLE_LEN
     // some date before the first leap seconds were inserted
     return leapsec_diff[0];
+}"""
+
+# ============= isleapsec_tai =============
+print
+
+print """bool isleapsec_tai(tai_t date)
+{
+    #define TABLE_LEN """ + str(len(times)) + """
+    time_t leapsec_dates[TABLE_LEN] = { 0 };
+    uint8_t leapsec_diff[TABLE_LEN] = { 0 };
+
+    #define new_leapsec(i, date, diff) leapsec_dates[i] = date; leapsec_diff[i] = diff;"""
+for i, leapsec in enumerate(times):
+    print '    new_leapsec(%d, %d, %s);' % (i, convert(leapsec[0]) + int(leapsec[1]) - 1, leapsec[1])
+
+print """    #undef new_leapsec
+
+    // go backwards since it's more likely that people will be using modern dates
+    for (int i = TABLE_LEN - 1; i >= 0; i--)
+    {
+        if (leapsec_dates[i] == date)
+        {
+            return true;
+        }
+    }
+    #undef TABLE_LEN
+    // not a leap second
+    return false;
 }"""
